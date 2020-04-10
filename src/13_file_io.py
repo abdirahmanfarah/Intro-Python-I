601d1ef564f97560a6a44777ee0d1ca3e94d2509
Abdi Farah <abdirahman.z.farah@gmail.com>
2020-04-09 17:00:19 -0700

    all files completed except cal

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,29 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+# with open('foo.txt') as f:
+#     for line in f:
+#         print(line, end='')
+
+# f.close()
+
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open('bar.txt', 'w') as f:
+    f.write("This is the first thing written \n")
+    f.write("This is the second thing written \n")
+    f.write("This is the third thing written \n")
+    f.closed
+
+
+with open('bar.txt') as f:
+    for line in f:
+        print(line, end='')
+
+f.close()
